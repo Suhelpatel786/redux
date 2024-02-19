c979af542faa4adb30898b44f070c73e4ec5f6df
Suhel Patel <suhel.softcolon@gmail.com>
2024-02-19 22:06:03 +0530

    use case for extra reducers

--- src/slice/bonusSlice.js
+++ src/slice/bonusSlice.js
@@ -1,8 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   points: 1,
 };
+const incrementByAmount = createAction("account/incrementByAmount");
 
 export const bonusSlice = createSlice({
   name: "bonus",
@@ -11,6 +12,15 @@ export const bonusSlice = createSlice({
     increment: (state) => {
       state.points += 1; //immer library used for it.
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(incrementByAmount, (state, action) => {
+      //when the below condition will fulfiled it will 1 in points current value
+      if (action.payload >= 100) {
+        state.points += 1;
+      }
+      return state;
+    });
   },
 });
 
